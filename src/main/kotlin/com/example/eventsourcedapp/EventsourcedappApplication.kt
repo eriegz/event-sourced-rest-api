e79f7d3008749c019576f34539c3138fb2730288
Evan Riegel <eriegz@gmail.com>
2023-03-26 11:04:37 -0600

    Initial file dump from Axon Initializr

---
+++ src/main/kotlin/com/example/eventsourcedapp/EventsourcedappApplication.kt
@@ -0,0 +1,11 @@
+package com.example.eventsourcedapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EventsourcedappApplication
+
+fun main(args: Array<String>) {
+	runApplication<EventsourcedappApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/eventsourcedapp/EventsourcedappApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.eventsourcedapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EventsourcedappApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
